73ab7b78c8042c051fba50337ca7bb73c1dbdf6a
brettmadrid <brettmadrid9@gmail.com>
2020-02-24 14:23:49 -0800

    created SHOP_DATA and imported in to ShopPage.js

--- src/pages/Shop/ShopPage.js
+++ src/pages/Shop/ShopPage.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import SHOP_DATA from "./ShopData";
+
 class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = [];
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    return <div>SHOP PAGE</div>;
   }
 }
 
