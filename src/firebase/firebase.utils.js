a2ae61111e9c38f7e48f3baa335fab28e0d04dbf
brettmadrid <brettmadrid9@gmail.com>
2020-03-31 16:05:33 -0700

    troubleshooting

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from "firebase/app"; // always required
+import "firebase/firestore"; // for the database
+import "firebase/auth"; // for auth
 
 const config = {
   apiKey: "AIzaSyB-vf4oZq6Ldy80szbklqdCn8D25Xb_pD8",
@@ -15,30 +15,31 @@ const config = {
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-  const snapshot = await userRef.get();
-  if (!snapshot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+  console.log(firestore.doc("users/128fdashdu"));
+  // const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // const snapshot = await userRef.get();
+  // if (!snapshot.exists) {
+  //   const { displayName, email } = userAuth;
+  //   const createdAt = new Date();
 
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
-    }
-  }
-  return userRef;
+  //   try {
+  //     await userRef.set({
+  //       displayName,
+  //       email,
+  //       createdAt,
+  //       ...additionalData
+  //     });
+  //   } catch (error) {
+  //     console.log("error creating user", error.message);
+  //   }
+  // }
+  // return userRef;
 };
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const auth = firebase.auth(); // exported out for any other component that needs auth
+export const firestore = firebase.firestore(); // export database access
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
